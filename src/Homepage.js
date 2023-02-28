134172946d4cf032b9daa5250e9591fb49bed08d
Dong Hyun Choi <dongandrewchoi@gmail.com>
2023-02-28 15:34:02 -0800

    added description, next step is to add patch notes and then add upcoming functionality

--- src/Homepage.js
+++ src/Homepage.js
@@ -11,7 +11,17 @@ function Homepage() {
         <div>Welcome Back {currUser.username}!</div>
       ) : (
         <div>
-          image time capsule, where you can forget memories for a while.
+          <div>
+            image time capsule, where you can forget memories for a while.
+          </div>
+          <div className="description">
+            Going through a breakup can be tough, and sometimes it's hard to let
+            go of the past. But holding onto old photos can be a painful
+            reminder of what's been lost. Our application is here to help. By
+            storing your photos securely, you can have the peace of mind that
+            comes with knowing they're safe and out of reach. Take the first
+            step towards moving on and start using our application today.
+          </div>
         </div>
       )}
     </div>
